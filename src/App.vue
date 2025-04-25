c2abad654fee8b707962ad5b4bc0fa206ed39a90
jlinares30 <jorgelinaresrrs306@hotmail.com>
2025-04-25 00:00:00 -0500

    fixed order of sections

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
   import HomeView from "@/views/HomeView.vue";
-  import CharacterView from "@/views/CharacterView.vue";
 </script>
 
 <template>
   <home-view/>
-  <character-view/>
 </template>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,13 @@ import NavbarSection from "@/components/NavbarSection.vue";
 import ContactComponent from "@/components/ContactComponent.vue";
 import TestimonialSection from "@/components/TestimonialSection.vue";
 import FooterSection from "@/components/FooterSection.vue";
+import CharacterView from "@/views/CharacterView.vue";
 </script>
 
 <template >
   <navbar-section/>
   <hero-section/>
+  <character-view/>
   <testimonial-section/>
   <contact-component/>
   <footer-section/>
